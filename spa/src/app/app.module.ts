652b08dd3980b0a007b305770dfc95dfc5223ecd
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 18:49:38 +0200

    feat(spa.app): insert the toast

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
