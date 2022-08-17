8223efd7534d37432826c02a5e4cd84ccdbab148
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 18:13:40 +0200

    feat(spa.containers): complete music-group-upload.component

--- spa/src/app/containers/music-group/smarts/music-group-upload/music-group-upload.component.ts
+++ spa/src/app/containers/music-group/smarts/music-group-upload/music-group-upload.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { MusicGroupUploadService } from '@shared/services/music-group-upload.service';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-music-group-upload',
   templateUrl: './music-group-upload.component.html',
   styleUrls: ['./music-group-upload.component.scss'],
 })
-export class MusicGroupUploadComponent implements OnInit {
-  constructor() {}
+export class MusicGroupUploadComponent implements OnDestroy {
+  private invalidMessageSubject = new BehaviorSubject<string | null>(null);
+  readonly invalidMessage$ = this.invalidMessageSubject.asObservable();
 
-  ngOnInit(): void {}
+  successMessage: string = '';
+
+  private subscription: Subscription = new Subscription();
+
+  constructor(private uploadService: MusicGroupUploadService) {}
+
+  onFileSelected(file: File): void {
+    this.invalidMessageSubject.next(null);
+
+    this.subscription = this.uploadService.upload(file).subscribe(
+      (response) => {
+        console.log(response);
+        this.successMessage = `Le fichier ${response.filename} a bien été téléchargé.`;
+      },
+      (error: HttpErrorResponse) => {
+        this.invalidMessageSubject.next(error.error.message);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
