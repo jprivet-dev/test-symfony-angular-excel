95415bbafca0fb455f5c8fbd66d716536c8a9ae2
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-18 00:20:01 +0200

    feat(api.controller): create MusicGroupDataController::delete()

--- api/src/Controller/MusicGroupDataController.php
+++ api/src/Controller/MusicGroupDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\MusicGroupData;
 use App\Repository\MusicGroupDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,12 +12,20 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class MusicGroupDataController extends AbstractController
 {
-    #[Route('/api/music-groups/data', name: 'app_music_group_data', methods: ['GET'])]
+    #[Route('/api/music-groups/data', name: 'app_music_groups_data_read', methods: ['GET'])]
     public function read(MusicGroupDataRepository $dataRepository, SerializerInterface $serializer): JsonResponse
     {
         $data = $dataRepository->findAll();
         $json = $serializer->serialize($data, 'json');
 
         return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/music-groups/data/{id}', name: 'app_music_groups_data_delete', methods: ['DELETE'])]
+    public function delete(MusicGroupData $data, MusicGroupDataRepository $dataRepository): JsonResponse
+    {
+        $dataRepository->remove($data, true);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
