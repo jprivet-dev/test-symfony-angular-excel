4c14a444e3fa10bf6a16487823deb93b2e04b131
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 23:27:23 +0200

    feat(spa.services): MusicGroupDataService > create load() et data$

--- spa/src/app/shared/services/music-group-data.service.ts
+++ spa/src/app/shared/services/music-group-data.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { MusicGroupData } from '@shared/music-group-data.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MusicGroupDataService {
+  private dataSubject = new BehaviorSubject<MusicGroupData[]>([]);
+  readonly data$ = this.dataSubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
   read(): Observable<MusicGroupData[]> {
     return this.http.get<MusicGroupData[]>(
       'https://localhost/api/music-groups/data'
     );
+  }
+
+  load(): Observable<MusicGroupData[]> {
+    return this.read().pipe(tap((response) => this.dataSubject.next(response)));
   }
 }
