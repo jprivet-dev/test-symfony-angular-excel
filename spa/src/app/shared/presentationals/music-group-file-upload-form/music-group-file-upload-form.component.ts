3bbfbd11604cfb6a815326e915cef8718d6b7b45
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 18:13:15 +0200

    feat(spa.shared.presentationals): complete music-group-file-upload-form

--- spa/src/app/shared/presentationals/music-group-file-upload-form/music-group-file-upload-form.component.ts
+++ spa/src/app/shared/presentationals/music-group-file-upload-form/music-group-file-upload-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-music-group-file-upload-form',
   templateUrl: './music-group-file-upload-form.component.html',
-  styleUrls: ['./music-group-file-upload-form.component.scss']
+  styleUrls: ['./music-group-file-upload-form.component.scss'],
 })
-export class MusicGroupFileUploadFormComponent implements OnInit {
+export class MusicGroupFileUploadFormComponent {
+  disabled: boolean = true;
+  file!: File;
 
-  constructor() { }
+  @ViewChild('fileUpload') fileUpload!: ElementRef;
+  @Input() invalidMessage: string | null = null;
+  @Output() fileSelectedEvent = new EventEmitter<File>();
 
-  ngOnInit(): void {
+  onFileSelected(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.disabled = target.files === null;
+
+    if (target.files) {
+      this.file = target.files[0];
+    }
   }
 
+  onSubmit(): void {
+    this.reset();
+    this.fileSelectedEvent.emit(this.file);
+  }
+
+  private reset() {
+    this.fileUpload.nativeElement.value = '';
+    this.disabled = true;
+  }
 }
