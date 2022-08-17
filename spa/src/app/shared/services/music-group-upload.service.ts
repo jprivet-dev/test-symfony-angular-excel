ef029bd445ac0212dfd729a479b5f068346d8659
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 18:12:48 +0200

    feat(spa.services): music-group-upload > complete upload()

--- spa/src/app/shared/services/music-group-upload.service.ts
+++ spa/src/app/shared/services/music-group-upload.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MusicGroupFileUploadModel } from '@shared/music-group-file-upload.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MusicGroupUploadService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  upload(file: File): Observable<MusicGroupFileUploadModel> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    console.log(formData);
+    return this.http.post<MusicGroupFileUploadModel>(
+      'https://localhost/api/music-groups/upload',
+      formData
+    );
+  }
 }
