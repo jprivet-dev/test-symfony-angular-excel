1f65f612661607cdbce32efe4111e2813de684e0
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-18 00:42:11 +0200

    feat(spa.containers): music-group-data.component > create delete button

--- spa/src/app/containers/music-group/smarts/music-group-data/music-group-data.component.ts
+++ spa/src/app/containers/music-group/smarts/music-group-data/music-group-data.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MusicGroupDataService } from '@shared/services/music-group-data.service';
+import { Subscription } from 'rxjs';
+import { MusicGroupData } from '@shared/music-group-data.model';
+import { ToastService } from '@shared/services/toast.service';
 
 @Component({
   selector: 'app-music-group-data',
   templateUrl: './music-group-data.component.html',
   styleUrls: ['./music-group-data.component.scss'],
 })
-export class MusicGroupDataComponent {
+export class MusicGroupDataComponent implements OnDestroy {
   readonly data$ = this.dataService.data$;
+  private deleteSubscription: Subscription = new Subscription();
 
-  constructor(private dataService: MusicGroupDataService) {}
+  constructor(
+    private dataService: MusicGroupDataService,
+    private toastService: ToastService
+  ) {}
 
   year(date: string): string {
     // TODO: cette donnée doit être renvoyée directement par l'API. Voir avec le serializer.
@@ -18,5 +25,19 @@ export class MusicGroupDataComponent {
 
   membres(membres: number): string {
     return membres > 0 ? membres.toString() : '';
+  }
+
+  delete(data: MusicGroupData): void {
+    if (confirm(`Souhaitez-vous supprimer le groupe "${data.nomDuGroupe}" ?`)) {
+      this.deleteSubscription = this.dataService.delete(data).subscribe(() => {
+        this.toastService.success(
+          `Le groupe "${data.nomDuGroupe}" a bien été supprimé`
+        );
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.deleteSubscription.unsubscribe();
   }
 }
