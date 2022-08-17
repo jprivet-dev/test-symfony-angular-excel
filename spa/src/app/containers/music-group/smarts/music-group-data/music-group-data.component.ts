956ffd6ca652151cea4b75225c977930d81947b6
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 23:56:53 +0200

    feat(spa.containers): complete music-group-data.component

--- spa/src/app/containers/music-group/smarts/music-group-data/music-group-data.component.ts
+++ spa/src/app/containers/music-group/smarts/music-group-data/music-group-data.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MusicGroupDataService } from '@shared/services/music-group-data.service';
 
 @Component({
   selector: 'app-music-group-data',
   templateUrl: './music-group-data.component.html',
-  styleUrls: ['./music-group-data.component.scss']
+  styleUrls: ['./music-group-data.component.scss'],
 })
-export class MusicGroupDataComponent implements OnInit {
+export class MusicGroupDataComponent {
+  readonly data$ = this.dataService.data$;
 
-  constructor() { }
+  constructor(private dataService: MusicGroupDataService) {}
 
-  ngOnInit(): void {
+  year(date: string): string {
+    // TODO: cette donnée doit être renvoyée directement par l'API. Voir avec le serializer.
+    return date ? date.split('-')[0] : '';
   }
 
+  membres(membres: number): string {
+    return membres > 0 ? membres.toString() : '';
+  }
 }
