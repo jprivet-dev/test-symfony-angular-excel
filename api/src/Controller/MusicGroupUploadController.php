296e674afb012a58c3e2c8a7a10fdb0ef4c4ee23
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 14:50:56 +0200

    feat(api.controller): MusicGroupUploadController::upload > return last uploaded file in json format

--- api/src/Controller/MusicGroupUploadController.php
+++ api/src/Controller/MusicGroupUploadController.php
@@ -8,13 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MusicGroupUploadController extends AbstractController
 {
     #[Route('/api/music-groups/upload', name: 'api_music_groups_upload', methods: ['POST'])]
-    public function upload(Request $request, MusicGroupFileUploadService $fileUpload): JsonResponse
-    {
+    public function upload(
+        Request $request,
+        MusicGroupFileUploadService $fileUpload,
+        SerializerInterface $serializer
+    ): JsonResponse {
         $requestFile = $request->files->get('file');
 
         $form = $this->createForm(MusicGroupFileUploadType::class);
@@ -30,7 +35,10 @@ class MusicGroupUploadController extends AbstractController
                 throw new InvalidArgumentException('file field does not empty.');
             }
 
-            $fileUpload->save($uploadedFile);
+            $file = $fileUpload->save($uploadedFile);
+            $json = $serializer->serialize($file, 'json');
+
+            return new JsonResponse($json, Response::HTTP_CREATED, [], true);
         }
 
         throw new InvalidArgumentException($form->getErrors(true));
