d4c95b32a55a0629adf6b97a0a9a6d69eeab0cd3
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-18 01:06:32 +0200

    feat(api.controller): create base of MusicGroupDataController::create()

--- api/src/Controller/MusicGroupDataController.php
+++ api/src/Controller/MusicGroupDataController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\MusicGroupData;
 use App\Repository\MusicGroupDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -27,5 +29,27 @@ class MusicGroupDataController extends AbstractController
         $dataRepository->remove($data, true);
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/music-groups/data', name: 'app_music_groups_data_create', methods: ['POST'])]
+    public function create(
+        Request $request,
+        MusicGroupDataRepository $dataRepository,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        $data = $serializer->deserialize($request->getContent(), MusicGroupData::class, 'json');
+
+        $alreadyExists = $dataRepository->findBy([
+            'nomDuGroupe' => $data->getNomDuGroupe(),
+        ]);
+
+        if (count($alreadyExists) > 0) {
+            throw new InvalidArgumentException(sprintf('Le groupe "%s" existe déjà.', $data->getNomDuGroupe()));
+        }
+
+        $dataRepository->add($data, true);
+        $json = $serializer->serialize($data, 'json');
+
+        return new JsonResponse($json, Response::HTTP_CREATED, [], true);
     }
 }
