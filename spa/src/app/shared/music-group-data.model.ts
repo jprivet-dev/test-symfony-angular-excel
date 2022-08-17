78b0525b9026ff3648070ece0bab5b2d658a4a58
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 22:36:47 +0200

    feat(spa.model): create MusicGroupData

---
+++ spa/src/app/shared/music-group-data.model.ts
@@ -0,0 +1,12 @@
+export interface MusicGroupData {
+  id: number;
+  nomDuGroupe: string;
+  origine: string;
+  ville: string;
+  anneeDebut: string;
+  anneeSeparation: string;
+  fondateurs: string;
+  membres: number;
+  courantMusical: string;
+  presentation: string;
+}
