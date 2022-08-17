491fb206f4a3cecc628c79c59ce88406b4da69bb
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 22:37:48 +0200

    feat(spa.containers): create base of music-group-data.component

--- spa/src/app/containers/music-group/music-group.module.ts
+++ spa/src/app/containers/music-group/music-group.module.ts
@@ -4,9 +4,10 @@ import { MusicGroupRoutingModule } from './music-group-routing.module';
 import { MusicGroupComponent } from './music-group.component';
 import { MusicGroupUploadComponent } from './smarts/music-group-upload/music-group-upload.component';
 import { SharedModule } from '@shared/shared.module';
+import { MusicGroupDataComponent } from './smarts/music-group-data/music-group-data.component';
 
 @NgModule({
-  declarations: [MusicGroupComponent, MusicGroupUploadComponent],
+  declarations: [MusicGroupComponent, MusicGroupUploadComponent, MusicGroupDataComponent],
   imports: [CommonModule, MusicGroupRoutingModule, SharedModule],
 })
 export class MusicGroupModule {}
---
+++ spa/src/app/containers/music-group/smarts/music-group-data/music-group-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-music-group-data',
+  templateUrl: './music-group-data.component.html',
+  styleUrls: ['./music-group-data.component.scss']
+})
+export class MusicGroupDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
