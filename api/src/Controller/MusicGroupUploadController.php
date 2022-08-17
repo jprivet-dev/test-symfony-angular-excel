b62b0fdb7e9cb4ca30f261f27e675c1b910793cd
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 10:48:59 +0200

    feat(api.controller): MusicGroupUploadController > complete upload()

--- api/src/Controller/MusicGroupUploadController.php
+++ api/src/Controller/MusicGroupUploadController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Form\MusicGroupFileUploadType;
+use App\Service\MusicGroupFileUploadService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MusicGroupUploadController extends AbstractController
 {
-    #[Route('/api/music-groups/upload', name: 'api_music_groups_upload')]
-    public function index(): JsonResponse
+    #[Route('/api/music-groups/upload', name: 'api_music_groups_upload', methods: ['POST'])]
+    public function upload(Request $request, MusicGroupFileUploadService $fileUpload): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MusicGroupUploadController.php',
+        $requestFile = $request->files->get('file');
+
+        $form = $this->createForm(MusicGroupFileUploadType::class);
+
+        $form->submit([
+            'file' => $requestFile,
         ]);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form->get('file')->getData();
+
+            if (!$uploadedFile) {
+                throw new InvalidArgumentException('file field does not empty.');
+            }
+
+            $fileUpload->save($uploadedFile);
+        }
+
+        throw new InvalidArgumentException($form->getErrors(true));
     }
 }
