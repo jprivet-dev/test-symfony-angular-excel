4f09e7f19bcea20cefd1de3ffda49fd5fa774757
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 23:57:14 +0200

    feat(spa.containers): music-group-upload.component > use MusicGroupDataService

--- spa/src/app/containers/music-group/smarts/music-group-upload/music-group-upload.component.ts
+++ spa/src/app/containers/music-group/smarts/music-group-upload/music-group-upload.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MusicGroupUploadService } from '@shared/services/music-group-upload.service';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ToastService } from '@shared/services/toast.service';
+import { MusicGroupDataService } from '@shared/services/music-group-data.service';
 
 @Component({
   selector: 'app-music-group-upload',
   templateUrl: './music-group-upload.component.html',
   styleUrls: ['./music-group-upload.component.scss'],
 })
-export class MusicGroupUploadComponent implements OnDestroy {
+export class MusicGroupUploadComponent implements OnInit, OnDestroy {
   private invalidMessageSubject = new BehaviorSubject<string | null>(null);
   readonly invalidMessage$ = this.invalidMessageSubject.asObservable();
 
-  private subscription: Subscription = new Subscription();
+  private uploadSubscription: Subscription = new Subscription();
+  private dataLoadSubscription: Subscription = new Subscription();
 
   constructor(
     private uploadService: MusicGroupUploadService,
+    private dataService: MusicGroupDataService,
     private toastService: ToastService
   ) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
 
   onFileSelected(file: File): void {
     this.invalidMessageSubject.next(null);
 
-    this.subscription = this.uploadService.upload(file).subscribe(
+    this.uploadSubscription = this.uploadService.upload(file).subscribe(
       (response) => {
         this.toastService.success(
           `Le fichier ${response.filename} a bien été téléchargé.`
         );
+
+        this.loadData();
       },
       (error: HttpErrorResponse) => {
         this.invalidMessageSubject.next(error.error.message);
@@ -35,7 +44,12 @@ export class MusicGroupUploadComponent implements OnDestroy {
     );
   }
 
+  private loadData() {
+    this.dataLoadSubscription = this.dataService.load().subscribe();
+  }
+
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.uploadSubscription.unsubscribe();
+    this.dataLoadSubscription.unsubscribe();
   }
 }
