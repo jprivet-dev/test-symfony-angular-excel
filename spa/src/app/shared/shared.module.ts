f6feba2141affc28f9c33a5549185fbf1f92227e
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-08-17 18:49:03 +0200

    feat(spa.shared.toast): create new toast service & component

--- spa/src/app/shared/shared.module.ts
+++ spa/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MusicGroupFileUploadFormComponent } from './presentationals/music-group-file-upload-form/music-group-file-upload-form.component';
+import { ToastsComponent } from './smarts/toasts/toasts.component';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [MusicGroupFileUploadFormComponent],
-  imports: [CommonModule],
-  exports: [MusicGroupFileUploadFormComponent],
+  declarations: [MusicGroupFileUploadFormComponent, ToastsComponent],
+  imports: [CommonModule, NgbToastModule],
+  exports: [MusicGroupFileUploadFormComponent, ToastsComponent],
 })
 export class SharedModule {}
---
+++ spa/src/app/shared/smarts/toasts/toasts.component.ts
@@ -0,0 +1,19 @@
+import { Component, TemplateRef } from '@angular/core';
+import { ToastService } from '@shared/services/toast.service';
+
+@Component({
+  selector: 'app-toasts',
+  templateUrl: './toasts.component.html',
+  styleUrls: ['./toasts.component.scss'],
+  host: {
+    class: 'toast-container position-fixed top-0 end-0 p-3',
+    style: 'z-index: 1200',
+  },
+})
+export class ToastsComponent {
+  constructor(public toastService: ToastService) {}
+
+  isTemplate(toast: any) {
+    return toast.textOrTpl instanceof TemplateRef;
+  }
+}
